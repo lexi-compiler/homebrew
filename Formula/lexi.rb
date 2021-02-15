28b026d5ea993771c7d3dd31976407459f6a17f4
Matt Moore <matt@mattmoore.io>
2021-02-15 17:51:10 -0600

    lexi-v0.1.0-dev.6

--- Formula/lexi.rb
+++ Formula/lexi.rb
@@ -1,9 +1,9 @@
 class Lexi < Formula
-  version "0.1.0-dev.5"
+  version "0.1.0-dev.6"
   desc "A multi-language and multi-target modular research compiler designed for easy modification with first-class plugin support."
   homepage "https://github.com/lexi-compiler/lexi"
-  url "https://github.com/lexi-compiler/lexi/releases/download/v0.1.0-dev.5/lexi-0.1.0-dev.5.tar.gz"
-  sha256 "00c9af0969865c76354aabeefa6de2ca91686231160241217fb097fdb4788a31"
+  url "https://github.com/lexi-compiler/lexi/releases/download/v0.1.0-dev.6/lexi-0.1.0-dev.6.tar.gz"
+  sha256 "fdc7cca211194c5dec7f8e95ffbbeb8a8a291af022e36cae0caa91bea6594571"
 
   def install
     bin.install "lexi"
